de1f1ca2d2970dcb074e4c72699bfb2f11f32491
Livarbesaru <liburdidaniele1998@gmail.com>
2023-06-20 16:07:50 +0200

    feat ( sse ): added server send events

--- src/main/java/com/message/bidirectional/controller/WebController.java
+++ src/main/java/com/message/bidirectional/controller/WebController.java
@@ -3,6 +3,11 @@ package com.message.bidirectional.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.time.LocalTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Controller
 @RequestMapping("/web")
@@ -10,5 +15,31 @@ public class WebController {
     @GetMapping("/home")
     public String home(){
         return "index";
+    }
+
+    /**
+     * Restituisce un flusso dati con nome .name(),
+     * ed il flusso dati sarà in .data()
+     * SseEmitter è il gestore del flusso dati,
+     * mentre ExecutorService ci permetter di creare un flusso asincrono
+     * */
+    @GetMapping("/stream-sse-mvc")
+    public SseEmitter streamSseMvc() {
+        SseEmitter emitter = new SseEmitter();
+        ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
+        sseMvcExecutor.execute(() -> {
+            try {
+                for (int i = 0; true; i++) {
+                    SseEmitter.SseEventBuilder event = SseEmitter.event()
+                            .data("SSE MVC - " + LocalTime.now().toString())
+                            .id(String.valueOf(i))
+                            .name("message");
+                    emitter.send(event);
+                }
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        });
+        return emitter;
     }
 }
