c910d40725dcad2b2570b23b7e621cf331d13f0d
Livarbesaru <liburdidaniele1998@gmail.com>
2023-06-20 11:32:22 +0200

    feat ( user notification ): added possibility to notify user

--- src/main/java/com/message/bidirectional/controller/WebSocketController.java
+++ src/main/java/com/message/bidirectional/controller/WebSocketController.java
@@ -3,8 +3,11 @@ package com.message.bidirectional.controller;
 import com.message.bidirectional.model.Message;
 import com.message.bidirectional.model.OutputMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
@@ -13,6 +16,12 @@ import java.util.Date;
 @Controller
 @Slf4j
 public class WebSocketController {
+    private SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired
+    public WebSocketController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
     /**MessageMapping indica il mapping del metodo,
@@ -21,5 +30,15 @@ public class WebSocketController {
     public OutputMessage send(Message message){
         String time = new SimpleDateFormat("YYYY/MM/dd HH:mm:ss").format(new Date());
         return new OutputMessage(message.getFrom(), message.getMessage(), time);
+    }
+
+    @MessageMapping("/chat/{idRequest}/{idOperator}")
+    /**MessageMapping indica il mapping del metodo,
+     * */
+    @SendTo("/topic/response/{idOperator}")
+    public OutputMessage sendToUser(@DestinationVariable int idRequest,@DestinationVariable int idOperator){
+        String time = new SimpleDateFormat("YYYY/MM/dd HH:mm:ss").format(new Date());
+        log.info("idOperator: {} ,idRequest: {}",idOperator,idRequest);
+        return new OutputMessage("server","request done "+idRequest,time);
     }
 }
